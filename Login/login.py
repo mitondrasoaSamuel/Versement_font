650e5ed0591ad919da85800533ea7998445f4744
Raphaël <raphael.nk.dev@gmail.com>
2024-02-29 01:35:26 +0300

    feat:login controle

--- Login/login.py
+++ Login/login.py
@@ -1,14 +1,40 @@
 from tkinter import *
 from tkinter import ttk, Tk
 from tkinter import messagebox
+import requests as req
+import tkinter as tk
+import os
+import sys
 
+# import configAPI
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")  # Add parent directory to the path
+from configAPI import API
+from utilisateur import utilisateur as User
 
 root = Tk()
 root.title("Connexion")
 root.geometry("400x300+450+200")
 root.resizable(False, False)
 
+def login():
+    userData = {"email": entre_mail.get(), "password": entre_mdp.get()}
+    res = req.post(API.USER_LOGIN, userData)
+    
+    if(res.status_code == 200):
+        data = res.json()
+        api = API()
+        api.set_token(data)
+        open_utilisateur_window()
+       
+    else: 
+        messagebox.showerror("Error", "Invalid email or password!")
 
+def open_utilisateur_window():
+    #close_current_window
+    root.destroy()
+    #open new window
+    User(tk.Toplevel())
+    
 lbl_titre = Label(root, text="Formulaire", font=("Sans serif", 25), bg="#091821", fg="white", borderwidth=3, relief= SUNKEN)
 lbl_titre.place(x=0,y=0, width=400)
 
@@ -25,7 +51,7 @@ entre_mdp.place(x=150,y=150, width=200,height=30)
 
 # Boutton
 
-btn_enrengistrer = Button(root, text="Connexion", font=("Arial", 16), bg="#FF4500", fg="white")
+btn_enrengistrer = Button(root, text="Connexion", font=("Arial", 16), bg="#FF4500", fg="white", command=login)
 btn_enrengistrer.place(x=150, y=200, width=200)
 
 
--- configAPI.py
+++ configAPI.py
@@ -1,3 +1,4 @@
+import os
 class API:
     API = "http://87.98.242.78:8081/api/"
     CLIENT_URL = API + "client"
@@ -7,3 +8,14 @@ class API:
 
     TOKEN = "1|Alquur2FPriNJ7mPrXxNSzswP4yDGGXRHBYKr4Jbc3d7af61"
     HEADER = {"Authorization" : f"Bearer {TOKEN}"}
+
+    def set_token(self, data):
+        os.environ['auth_token_abda'] = data['auth_token']
+        self.HEADER = {"Authorization" : f"Bearer {self.get_token}"}
+        
+    def get_token(self):
+        return os.environ.get('auth_token_abda')
+    
+    def get_header(self):
+        return self.HEADER
+     
